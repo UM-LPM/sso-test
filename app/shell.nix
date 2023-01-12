9f710fd654b38cb894712e9557878e41af451bd9
Žiga Leber <zigaleber7@gmail.com>
2023-01-12 17:30:09 +0100

    Add login

--- app/shell.nix
+++ app/shell.nix
@@ -1,7 +1,5 @@
-
 let
   pkgs = import <nixpkgs> {
-     system = "x86_64-linux";
     config.allowUnfree = true;
     config.android_sdk.accept_license = true;
   };
@@ -9,34 +7,30 @@ let
     toolsVersion = "26.1.1";
     platformToolsVersion = "33.0.3";
     buildToolsVersions = [ "30.0.3" ];
-    #includeEmulator = false;
-    #emulatorVersion = "30.3.4";
-    platformVersions = ["28" "29" "30" "31"];
-    #includeSources = false;
-    #includeSystemImages = false;
-    #systemImageTypes = [ "google_apis_playstore" ];
-    #abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-    #cmakeVersions = [ "3.10.2" ];
-
+    includeEmulator = false;
+    platformVersions = ["30" "31"];
+    includeSources = false;
+    includeSystemImages = false;
+    abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+    cmakeVersions = ["3.10.2"];
     includeNDK = true;
-    #ndkVersions = ["22.0.7026061"];
-    #useGoogleAPIs = false;
-    #useGoogleTVAddOns = false;
-    #includeExtras = [
-    #  "extras;google;gcm"
-    #];
+    ndkVersions = ["25.1.8937393"];
+    useGoogleAPIs = false;
+    useGoogleTVAddOns = false;
   };
+  gradle = pkgs.gradle.override { java = pkgs.jdk11;};
 in
 pkgs.mkShell rec {
-  buildInputs = [pkgs.flutter android.platform-tools pkgs.jdk11 pkgs.dart ];
+  buildInputs = [pkgs.flutter android.platform-tools pkgs.jdk11 gradle pkgs.dart];
 
   ANDROID_SDK_ROOT = "${android.androidsdk}/libexec/android-sdk";
   ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
   JAVA_HOME = pkgs.jdk11;
 
-  #GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/30.0.3/aapt2";
+  shellHook = ''
+    rm -rf /tmp/gradle &> /dev/null
+    mkdir /tmp/gradle 
+    export GRADLE_USER_HOME="/tmp/gradle" 
+    echo "org.gradle.java.home=${pkgs.jdk11}/lib/openjdk" > /tmp/gradle/gradle.properties
+  '';
 }
-
-  #ANDROID_HOME = "${android.androidsdk}/libexec/android-sdk";
-  #JAVA_HOME = pkgs.jdk;
-  #ANDROID_AVD_HOME = (toString ./.) + "/.android/avd";
